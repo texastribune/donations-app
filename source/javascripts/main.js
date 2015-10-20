8bd3a75a82403c5de1af0b6eb4c440933ce7ad51
Kathryn Beaty <kabeaty@gmail.com>
2015-10-20 11:07:03 -0500

    Update bottom checkout js to send correct info through to stripe

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -206,38 +206,37 @@ $(document).ready(function() {
     // Don't submit the form as is
     e.preventDefault();
 
-    // Base Givalike url
-    var url = "https://givalike.org/public/quickgive.aspx?cid=";
+    var installmentPeriod, installments;
 
-    var form_cid = 227;
+    // Base url for form
+    var url = "https://support.texastribune.org/form?";
 
     // Grab contribution amount
     var amount = $('#spinner-bottom').val();
 
-    // Get frequency and set if monthly or not
-    get_frequency_bottom();
+     // Get frequency and set if monthly or yearly
+    get_frequency();
     if (is_monthly) {
-      monthly = 1;
+      installmentPeriod = 'monthly';
     } else {
-      monthly = 0;
+      installmentPeriod = 'yearly';
     }
 
-    // if circle, send to different form
+    // Set up installments if circle
     if (is_monthly && amount > 83) {
-      form_cid = 231;
+      installments = 36;
     } else if (!is_monthly && amount > 999) {
-      form_cid = 231;
+      installments = 3;
     }
 
-    // Add cid and params to url
-    url += form_cid;
+    // Add installments if circle
+    if (installments) {
+      url += '&installments=';
+      url += installments;
+    }
 
-    // Add params to url
-    url += '&amount=';
-    url += amount;
-
-    url += '&monthly=';
-    url += monthly;
+    url += '&installmentPeriod=';
+    url += installmentPeriod;
 
     $('#contribution-form-bottom').get(0).setAttribute('action', url);
 
