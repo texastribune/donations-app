2ea70c990f0f66d72b6c8f6b9860973172e3fd98
anniedaniel <adaniel@texastribune.org>
2016-04-22 17:38:12 -0500

    building tables more efficiently

--- source/javascripts/donor-wall.js
+++ source/javascripts/donor-wall.js
@@ -29,21 +29,40 @@ var slug = function(str) {
 function buildWall(data) {
 	var nameList = [];
 	// initial each loop to grab name
+	var wall = '<table class="donor-wall-table">' +
+					'<thead>' +
+						'<tr>' +
+							'<th><div class="label sponsor">Sponsor</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>' +
+							'<th><div class="label">Amount</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>' +
+						'</tr>' +
+					'</thead>' +
+					'<tbody class="donor-wall-tbody">' +
+					'</tbody>' +
+				'</table>';
+	var $processWall = $(wall);
 	$.each(data, function (index, objects) {
 		var sponsorName = objects.name;
 		var slugName = slug(sponsorName);
-		var sponsorRow = '<tr>'+
+		var $sponsorRow = '<tr>'+
 							'<td>'+ sponsorName +'</td>' +
 							'<td class="amount '+ slugName +'"></td>'+
-							'</tr>';
-		$('table').append(sponsorRow);
+						'</tr>';
+		var $processSponsorRow = $($sponsorRow);
+		$processWall.find('tbody').html($processSponsorRow);
+		// $($wall + 'tbody').append($sponsorRow);
+		// $('tbody').append(sponsorRow);
 		// console.log(objects.donations);
 		// console.log(index);
 		// second each loop to grab donations
 		$.each(objects.donations, function(type, i) {
-			$('.' + slugName).append(i.amount);
-			// console.log('hi');
+			if (i.year == 'all-time') {
+				$processSponsorRow.find('.'+slugName).html(i.amount); 
+				// $($sponsorRow +'.'+slugName).append(i.amount);
+				// $('.' + slugName).append(i.amount); 
+			}
 		});
 	});
+	$('.donor-wall-table').html($processWall);
+	console.log($processWall);
 
 }
