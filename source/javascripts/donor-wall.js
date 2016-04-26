cb1d3949fa2991f9d5e3f73775f15179c761a84a
anniedaniel <adaniel@texastribune.org>
2016-04-26 10:41:14 -0500

    year dropdown

--- source/javascripts/donor-wall.js
+++ source/javascripts/donor-wall.js
@@ -1,15 +1,5 @@
 var $donorJSON = 'http://membership.texastribune.org/donors.json';
 
-// var ajax = new XMLHttpRequest();
-// ajax.open("GET", 'http://membership.texastribune.org/donors.json', true);
-// ajax.onload = function() {
-// 	var list = JSON.parse(ajax.responseText).map(function(i) { 
-// 		return i.name; 
-// 	});
-// 	new Awesomplete(Awesomplete.$("#ajax-example"),{ list: list });
-// };
-// ajax.send();
-
 $.ajax({
   url: $donorJSON,
   type: 'GET',
@@ -17,6 +7,7 @@ $.ajax({
   success: buildWall
 });
 
+//slugifies names for table row classes
 var slug = function(str) {
     var $slug = '';
     var trimmed = $.trim(str);
@@ -28,6 +19,8 @@ var slug = function(str) {
 
 function buildWall(data) {
 	var nameList = [];
+	// all time is a space holder here, it gets added on the page
+	var yearList = ['all-time'];
 	// initial each loop to grab name
 	var wall = '<table class="donor-wall-table">' +
 					'<thead>' +
@@ -43,21 +36,36 @@ function buildWall(data) {
 	$.map(data, function(val, i) {
 		var sponsorName = val.name,
 			slugName = slug(sponsorName),
-			slugClass = '.' + slugName,
+			// tablerow for each sponsor with their name and ammount
 			$sponsorRow = '<tr>'+
 							'<td>'+ sponsorName +'</td>' +
 							'<td class="amount '+ slugName +'"></td>'+
 						  '</tr>',
+			// allows $sponsorRow to be callable before getting placed on the DOM
 			$processSponsorRow = $($sponsorRow);
+		// finds the wall and adds the new table row
 		$processWall.find('tbody').append($processSponsorRow);
+		// pushes sponsor name to list used by Awesomplete
 		nameList.push(sponsorName);
+		// loops through each person object to access their donations
 		$.map(val.donations, function(amount, index) {
+			if ($.inArray(amount.year, yearList) === -1) {
+				yearList.push(amount.year);
+			}
+			// grabs all time amounts for load
 			if (amount.year == 'all-time') { 
 				$processSponsorRow.find('.amount').append(amount.amount); 
 			}
 		});
 	});
+	yearList.sort();
+	$.map(yearList, function(value, index) {
+		$objectList = '<option class="'+ value +'-year">'+ value +'</option>';
+		$('.year-select').append($objectList);
+	});
+	console.log(yearList);
+	// creates Awesomplete instance
 	new Awesomplete(Awesomplete.$("#ajax-example"),{ list: nameList });
-
+	// sticks table on the page once it's already been put together
 	$('.donor-wall-table').html($processWall);
 }
