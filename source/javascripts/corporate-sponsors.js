99bf3854502354d022446368e37cb8b8c698bc49
anniedaniel <adaniel@texastribune.org>
2016-04-29 15:18:00 -0500

    donor wall table built with just all time json, working awesomplete search

--- source/javascripts/corporate-sponsors.js
+++ source/javascripts/corporate-sponsors.js
@@ -1,124 +1,124 @@
-var $sponsorJSON = 'http://membership.texastribune.org/sponsors.json';
+// var $sponsorJSON = 'http://membership.texastribune.org/sponsors.json';
 
-$.ajax({
-  url: $sponsorJSON,
-  type: 'GET',
-  dataType: 'json',
-  success: buildTable
-});
+// $.ajax({
+//   url: $sponsorJSON,
+//   type: 'GET',
+//   dataType: 'json',
+//   success: buildTable
+// });
 
-function slug(str) {
-    var $slug = '';
-    var trimmed = $.trim(str);
-    $slug = trimmed.replace(/[^a-z0-9-]/gi, '-').
-    replace(/-+/g, '-').
-    replace(/^-|-$/g, '');
-    return $slug.toLowerCase();
-}
+// function slug(str) {
+//     var $slug = '';
+//     var trimmed = $.trim(str);
+//     $slug = trimmed.replace(/[^a-z0-9-]/gi, '-').
+//     replace(/-+/g, '-').
+//     replace(/^-|-$/g, '');
+//     return $slug.toLowerCase();
+// }
 
-function buildTable(data){
-  var tableRow = '';
-  var year = '';
-  var corporateNames = [];
-  // initial each loop to grab years
-  $.map(data, function(val, i) {
-    var displayYear;
+// function buildTable(data){
+//   var tableRow = '';
+//   var year = '';
+//   var corporateNames = [];
+//   // initial each loop to grab years
+//   $.map(data, function(val, i) {
+//     var displayYear;
 
-    if (i === 'all-time') {
-      displayYear = 'All time';
-    } else {
-      displayYear = i;
-    }
+//     if (i === 'all-time') {
+//       displayYear = 'All time';
+//     } else {
+//       displayYear = i;
+//     }
 
-    yearOption = '<option class="year-option year-'+ i +'">'+ displayYear +'</option>';
-    // only adds a year to the select box option list if it's a year
-    // All time is hard-coded into the select box so it will always appear first
-    if (displayYear != 'All time') {
-      $('.year-select').append(yearOption);
-    }
+//     yearOption = '<option class="year-option year-'+ i +'">'+ displayYear +'</option>';
+//     // only adds a year to the select box option list if it's a year
+//     // All time is hard-coded into the select box so it will always appear first
+//     if (displayYear != 'All time') {
+//       $('.year-select').append(yearOption);
+//     }
 
-    year = '<table class="donor-table'+i+' table-year-'+i+' donor-table">' + 
-              '<thead>' +
-                '<tr class="labels">'+
-                    '<th><div class="label">Sponsor</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
-                    '<th><div class="label">Digital revenue</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
-                    '<th><div class="label">Digital in-kind</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
-                    '<th><div class="label">Events revenue</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
-                    '<th><div class="label">Events in-kind</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
-                    '<th><div class="label">Total</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
-                  '</tr>'+
-                '</thead>'+
-              '<tbody></tbody>'+
-            '</table>';
+//     year = '<table class="donor-table'+i+' table-year-'+i+' donor-table">' + 
+//               '<thead>' +
+//                 '<tr class="labels">'+
+//                     '<th><div class="label">Sponsor</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
+//                     '<th><div class="label">Digital revenue</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
+//                     '<th><div class="label">Digital in-kind</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
+//                     '<th><div class="label">Events revenue</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
+//                     '<th><div class="label">Events in-kind</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
+//                     '<th><div class="label">Total</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>'+
+//                   '</tr>'+
+//                 '</thead>'+
+//               '<tbody></tbody>'+
+//             '</table>';
 
-    $('.year').append(year);
-    // loops through each donation for $(this) year
-    $.map(val, function(donation, index) {
-      var sponsor = donation.sponsor;
-      var sponsorSlug = slug(sponsor);
-      // only adds corporateNames to the list once
-      if (i === 'all-time') {
-        corporateNames.push(sponsor);
-      }
-      // if there's a NULL or 0 in the data- just display empty table cells
-      if (sponsor == 'NULL') {
-        sponsor = '';
-      }
-      var digitalRevenue = donation.digital_revenue;
-      if (donation.digital_revenue == '$0'){
-        digitalRevenue = '';
-      }
-      var digitalInKind = donation.digital_in_kind;
-      if (donation.digital_in_kind == '$0'){
-        digitalInKind = '';
-      }
-      var eventsRevenue = donation.events_revenue;
-      if (donation.events_revenue == '$0'){
-        eventsRevenue = '';
-      }
-      var eventsInKind = donation.events_in_kind;
-      if (donation.events_in_kind == '$0'){
-        eventsInKind = '';
-      }
-      var total = donation.total;
+//     $('.year').append(year);
+//     // loops through each donation for $(this) year
+//     $.map(val, function(donation, index) {
+//       var sponsor = donation.sponsor;
+//       var sponsorSlug = slug(sponsor);
+//       // only adds corporateNames to the list once
+//       if (i === 'all-time') {
+//         corporateNames.push(sponsor);
+//       }
+//       // if there's a NULL or 0 in the data- just display empty table cells
+//       if (sponsor == 'NULL') {
+//         sponsor = '';
+//       }
+//       var digitalRevenue = donation.digital_revenue;
+//       if (donation.digital_revenue == '$0'){
+//         digitalRevenue = '';
+//       }
+//       var digitalInKind = donation.digital_in_kind;
+//       if (donation.digital_in_kind == '$0'){
+//         digitalInKind = '';
+//       }
+//       var eventsRevenue = donation.events_revenue;
+//       if (donation.events_revenue == '$0'){
+//         eventsRevenue = '';
+//       }
+//       var eventsInKind = donation.events_in_kind;
+//       if (donation.events_in_kind == '$0'){
+//         eventsInKind = '';
+//       }
+//       var total = donation.total;
 
-      // fill in each year's table with it's respective data
-      tableRow = '<tr class="'+ sponsorSlug +'">'+
-                    '<td class="sponsor"><a href="' + donation.url + '">' + sponsor + '</a></td>'+
-                    '<td>' + digitalRevenue + '</td>'+
-                    '<td>' + digitalInKind + '</td>'+
-                    '<td>' + eventsRevenue + '</td>'+
-                    '<td>' + eventsInKind + '</td>'+
-                    '<td>' + total + '</td>'+
-                  '</tr>';
-      $('.donor-table'+i).append(tableRow);
-    });
-  });
-  // display all time table on load as default
-  $('.table-year-all-time').show();
+//       // fill in each year's table with it's respective data
+//       tableRow = '<tr class="'+ sponsorSlug +'">'+
+//                     '<td class="sponsor"><a href="' + donation.url + '">' + sponsor + '</a></td>'+
+//                     '<td>' + digitalRevenue + '</td>'+
+//                     '<td>' + digitalInKind + '</td>'+
+//                     '<td>' + eventsRevenue + '</td>'+
+//                     '<td>' + eventsInKind + '</td>'+
+//                     '<td>' + total + '</td>'+
+//                   '</tr>';
+//       $('.donor-table'+i).append(tableRow);
+//     });
+//   });
+//   // display all time table on load as default
+//   $('.table-year-all-time').show();
 
-  // year dropdown functionality
-  $('.year-select').change(function() {
-    var yearSelect = $(this).val();
-    var yearClass = '.table-year-'+yearSelect;
-    $('.donor-table').hide();
-    if (yearSelect === 'All time') {
-      yearClass = '.table-year-all-time';
-    }
-    $(yearClass).show();
-  }); 
+//   // year dropdown functionality
+//   $('.year-select').change(function() {
+//     var yearSelect = $(this).val();
+//     var yearClass = '.table-year-'+yearSelect;
+//     $('.donor-table').hide();
+//     if (yearSelect === 'All time') {
+//       yearClass = '.table-year-all-time';
+//     }
+//     $(yearClass).show();
+//   }); 
 
-  new Awesomplete(Awesomplete.$("#corporate-search"),{ list: corporateNames });
+//   new Awesomplete(Awesomplete.$("#corporate-search"),{ list: corporateNames });
 
-  $('#go').click(function() {
-    var value = $('#corporate-search').val();
-    var slugValue = slug(value);
-    $('tr').hide();
-    $('.'+slugValue).show();
-    $('.labels').show();
-  });
+//   $('#go').click(function() {
+//     var value = $('#corporate-search').val();
+//     var slugValue = slug(value);
+//     $('tr').hide();
+//     $('.'+slugValue).show();
+//     $('.labels').show();
+//   });
 
-  $('#reset').click(function() {
-    $('tr').show();
-  });
-}
+//   $('#reset').click(function() {
+//     $('tr').show();
+//   });
+// }
--- source/javascripts/donor-wall.js
+++ source/javascripts/donor-wall.js
@@ -1,4 +1,4 @@
-var $donorJSON = 'http://membership.texastribune.org/donors.json';
+var $donorJSON = 'https://membership.texastribune.org/all_time.json';
 
 $.ajax({
   url: $donorJSON,
@@ -19,10 +19,9 @@ var slug = function(str) {
 
 function buildWall(data) {
 	var nameList = [];
-	var yearList = [];
 	var wall = '<table class="donor-wall-table">' +
 					'<thead>' +
-						'<tr>' +
+						'<tr class="labels">' +
 							'<th><div class="label sponsor">Sponsor</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>' +
 							'<th><div class="label">Amount</div><span class="fa-stack"><i class="sort-up fa fa-caret-up fa-stack-1x"></i><i class="sort-down fa fa-caret-down fa-stack-1x"></i></span></th>' +
 						'</tr>' +
@@ -36,10 +35,11 @@ function buildWall(data) {
 	$.map(data, function(val, i) {
 		var sponsorName = val.name,
 			slugName = slug(sponsorName),
+			sponsorAmount = val.amt,
 			// tablerow for each sponsor with their name and slot for ammount
-			$sponsorRow = '<tr>'+
+			$sponsorRow = '<tr class="'+ slugName +'">'+
 							'<td>'+ sponsorName +'</td>' +
-							'<td class="amount '+ slugName +'"></td>'+
+							'<td class="amount '+ slugName +'">'+sponsorAmount+'</td>'+
 						  '</tr>',
 			// allows $sponsorRow to be callable before getting placed on the DOM
 			$processSponsorRow = $($sponsorRow);
@@ -48,39 +48,23 @@ function buildWall(data) {
 		// pushes sponsor name to list used by Awesomplete
 		nameList.push(sponsorName);
 		// loops through each donations object to access donation amounts and years
-		$.map(val.donations, function(amount, index) {
-			if ($.inArray(amount.year, yearList) === -1) {
-				yearList.push(amount.year);
-			}
-			// appends all donations to table and adds a year class
-			$processSponsorRow.find('.amount').append(amount.amount); 
-			$processSponsorRow.find('.amount').addClass(amount.year);
-		});
-	});
-
-	yearList.sort();
-	// gets rid of the all-time collected from salesforce 
-	// because it goes to the bottom of the list on sort
-	// All time is hard-coded on the page so it's always at the top
-	yearList.splice(-1);
-	// builds year dropdown
-	$.map(yearList, function(value, index) {
-		$objectList = '<option class="'+ value +'-year">'+ value +'</option>';
-		$('#year-select').append($objectList);
 	});
 	// creates Awesomplete instance
 	new Awesomplete(Awesomplete.$("#donor-search"),{ list: nameList });
 	// sticks table on the page once it's already been put together
 	$('.donor-wall-table').html($processWall);
+
+	$('#go').click(function() {
+		console.log('click');
+		var value = $('#donor-search').val();
+		var slugValue = slug(value);
+		$('tr').hide();
+		$('.'+slugValue).show();
+		$('.labels').show();
+	});
+
+	$('#reset').click(function() {
+	  $('tr').show();
+	});
 }
-
-console.log($('.donor-wall-table'));			
-
-$('.donor-wall-table').on('click', function() {
-	alert('CLICK');
-});
-
-$('select').on('change',function() {
-	alert('HEY');
-});
 
