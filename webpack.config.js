e7eadc3920cbb1c825e5f60f103f20e74316fb37
AndrewGibson27 <atgibson27@gmail.com>
2017-03-10 14:37:15 -0600

    refactor with support for background images

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: './source/javascripts/index.js',
+  entry: './raw/js/index.js',
 
   output: {
-    path: __dirname + '/.tmp/dist',
+    path: path.join(__dirname, 'source'),
     filename: 'javascripts/bundle.js',
-    publicPath: '/'
+    publicPath: './'
   },
 
   resolve: {
@@ -17,19 +17,22 @@ module.exports = {
       path.join(__dirname, 'source')
     ],
 
-    extensions: ['.js', '.css', '.scss']
+    extensions: ['.js']
   },
 
   plugins: [
-    new ExtractTextPlugin('styles.css')
+    new ExtractTextPlugin({
+      filename: 'css/styles.css'
+    })
   ],
 
   module: {
     rules: [
       {
         test: /\.scss$/,
-        include: [/source/, /node_modules/],
+        include: [/raw/, /source/, /node_modules/],
         use: ExtractTextPlugin.extract({
+          publicPath: '../bg/',
           fallback: 'style-loader',
           use: [{
             loader: 'css-loader'
@@ -47,11 +50,27 @@ module.exports = {
 
       {
         test: /\.js$/,
-        include: [/source/, /node_modules/],
+        include: [/raw/, /source/, /node_modules/],
         use: [{
           loader: 'babel-loader',
           options: { presets: ['es2015'] }
-        }],
+        }]
+      },
+
+      {
+        test: /\.(jpg|png|gif|webp)$/,
+        include: [
+          /raw/,
+          /source\/scss/,
+          /source\/bg/,
+          /node_modules/
+        ],
+        exclude: [
+          /source\/img/,
+        ],
+        use: [{
+          loader: 'file-loader?name=[name].[ext]&outputPath=bg/'
+        }]
       }
     ]
   }
