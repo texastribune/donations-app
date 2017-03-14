a6127cc7ee4be39c3944017d9509ce3b9a4c6188
AndrewGibson27 <atgibson27@gmail.com>
2017-03-14 18:08:36 -0500

    add carousel initialization tests

--- test/form-tests.js
+++ test/form-tests.js
@@ -1,26 +1,68 @@
-import chai from 'chai';
-import sinon from 'sinon';
-
+const chai = require('chai');
+const sinon = require('sinon');
 const assert = chai.assert;
 const expect = chai.expect;
 
 import FormHandler from '../raw/js/form';
 
 describe('Donation carousel form', () => {
-  let DonationForm = new FormHandler();
+  let DonationForm;
 
-  afterEach(() => {
-    DonationForm = new FormHandler();
+  beforeEach(function(){
+    this.jsdom = require('jsdom-global')();
+    window.jQuery = window.$ = global.$ = require('jquery');
+
+    DonationForm = new FormHandler({
+      rangeAttach: $('<div/>'),
+      amountsAttach: $('<div/>'),
+      outerContainer: $('<div/>'),
+      innerContainer: $('<div/>'),
+      carouselSlides: $('<div/><div/><div/>'),
+      prevButton: $('<button/>'),
+      nextButton: $('<button/>'),
+      submitButton: $('<input type="submit">'),
+      startFrequencyIndex: 1,
+      startRangeIndex: 1,
+      startSlide: 0,
+      frequencyToRanges: [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+      ],
+      rangesToAmounts: [
+        [13, 14, 15, 16],
+        [17, 18, 19, 20],
+        [21, 22, 23, 24]
+      ]
+    });
   });
 
-  it('should be the width of its container times the number of slides', () => {
-    const getOuterContainerWidth = sinon.stub(DonationForm, 'getOuterContainerWidth');
+  it('inner container should be outer container width times number of slides', () => {
+    sinon.stub(DonationForm, 'getOuterContainerWidth').returns(100);
+    DonationForm.setInnerContainerWidth();
+    assert.equal(DonationForm.innerContainer.css('width'), '300px');
+  });
 
-    DonationForm.carouselInner = $('<div/>');
-    DonationForm.numSlides = 3;
-    getOuterContainerWidth.returns(100);
+  it('slide width should be equal to outer container width', () => {
+    sinon.stub(DonationForm, 'getOuterContainerWidth').returns(100);
+    DonationForm.setSlideWidth();
+    assert.equal(DonationForm.carouselSlides.css('width'), '100px');
+  });
 
-    DonationForm.setInnerWidth();
-    assert.equal(DonationForm.carouselInner.css('width'), '300px');
+  it('transform amount should be equal to current slide index times outer container width', () => {
+    sinon.stub(DonationForm, 'getOuterContainerWidth').returns(100);
+    DonationForm.currSlide = 2;
+    DonationForm.setTransform();
+    assert.equal(DonationForm.innerContainer.css('transform'), 'translateX(-200px)');
+  });
+
+  it('transform amount should never be negative', () => {
+    sinon.stub(DonationForm, 'getOuterContainerWidth').returns(100);
+
+    DonationForm.currSlide = 0;
+    assert.isAtLeast(DonationForm.getTransformValue(), 0);
+
+    DonationForm.currSlide = 2;
+    assert.isAtLeast(DonationForm.getTransformValue(), 0);
   });
 });
