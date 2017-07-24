2d59416d0faf5b3f76d4ede983be0f1df6d34039
AndrewGibson27 <atgibson27@gmail.com>
2017-07-24 16:18:20 -0500

    better webpack display options

--- config.rb
+++ config.rb
@@ -15,19 +15,12 @@ end
 
 activate :gzip
 
-activate :s3_sync do |config|
-  config.bucket = 'support.texastribune.org'
-  config.region = 'us-east-1'
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY']
-  config.aws_secret_access_key = ENV['AWS_ACCESS_SECRET']
-end
-
 # https://rossta.net/blog/using-webpack-with-middleman.html
 activate :external_pipeline,
   name: :webpack,
   command: build? ?
-  "./node_modules/webpack/bin/webpack.js --bail -p" :
-  "./node_modules/webpack/bin/webpack.js --colors --watch -p",
+  "./node_modules/webpack/bin/webpack.js --bail --display errors-only" :
+  "./node_modules/webpack/bin/webpack.js --watch",
   source: ".tmp",
   latency: 1
 
