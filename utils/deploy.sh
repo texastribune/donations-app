7f17eadd96036028b60b1aace9bbb497680b4164
AndrewGibson27 <atgibson27@gmail.com>
2017-07-26 17:58:54 -0500

    finalize s3 deployment

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -5,16 +5,16 @@ echo "S3 BUCKET: "${npm_package_config_s3_bucket:?"The S3 bucket needs to be set
 export APP_S3_BUCKET=$npm_package_config_s3_bucket
 
 echo "Syncing *.css files to S3..."
-aws s3 sync --acl public-read --include '*.css' --cache-control 'max-age=31536000' build s3://$APP_S3_BUCKET/
+aws s3 sync --acl public-read --delete --include '*.css' --cache-control 'max-age=31536000' build s3://$APP_S3_BUCKET/
 
 echo "Syncing *.js files to S3..."
-aws s3 sync --acl public-read --include '*.js' --cache-control 'max-age=31536000' build s3://$APP_S3_BUCKET/
+aws s3 sync --acl public-read --delete --include '*.js' --cache-control 'max-age=31536000' build s3://$APP_S3_BUCKET/
 
 echo "Syncing *.html files to S3..."
-aws s3 sync --acl public-read --include '*.html' --cache-control 'max-age=60' build s3://$APP_S3_BUCKET/
+aws s3 sync --acl public-read --delete --include '*.html' --cache-control 'max-age=60' build s3://$APP_S3_BUCKET/
 
 echo "Syncing image files to S3..."
-aws s3 sync --acl public-read --include '*.jpg' --include '*.png' --include '*.gif' --cache-control 'max-age=604800' build s3://$APP_S3_BUCKET/
+aws s3 sync --acl public-read --delete --include '*.jpg' --include '*.png' --include '*.gif' --cache-control 'max-age=604800' build s3://$APP_S3_BUCKET/
 
 echo "Syncing everything else to S3..."
-aws s3 sync --profile newsapps build s3://$APP_S3_BUCKET/
+aws s3 sync --dryrun build s3://$APP_S3_BUCKET/
